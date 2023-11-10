f5369c7a35e34c74488a88ae15a91cfb24893a87
jakebalula <balula@hartford.edu>
2023-11-09 21:56:44 -0500

    starting array

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,21 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int row;
+    int col;
+  {
+    int [][] table = new int [5] [10];
+
+    for (row = 0; row < table.length; row++)
+      for (col = 0; col < table[row].length; col++)
+        table[row][col] = row * 10 + col;
+
+    for (row = 0; row < table.length; row++)
+    {
+      for (col = 0; col < table[row].length; col++)
+        System.out.print(table[row][col] + "\t");
+      System.out.println();
+    }
+  }
   }
 }
