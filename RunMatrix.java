ca4f0a0281ae0dcc18a4233b56d6664876a43bc3
jakebalula <balula@hartford.edu>
2023-11-11 17:24:02 -0500

    first matrix

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,12 +4,16 @@ public class RunMatrix {
   public static void main(String[] args) {
     int row;
     int col;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter the size of your matrix");
+    int size = input.nextInt();
+    System.out.println("Your matrix is " + size + " x " + size);
   {
-    int [][] table = new int [5] [10];
+    int [][] table = new int [size] [size];
 
-    for (row = 0; row < table.length; row++)
-      for (col = 0; col < table[row].length; col++)
-        table[row][col] = row * 10 + col;
+    // for (row = 0; row < table.length; row++)
+    //   for (col = 0; col < table[row].length; col++)
+    //     table[row][col] = row * 10 + col;
 
     for (row = 0; row < table.length; row++)
     {
