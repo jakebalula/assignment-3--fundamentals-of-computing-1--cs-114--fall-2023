6d924df3c41458a021f81908a27ca75b173a013c
jakebalula <balula@hartford.edu>
2023-11-11 18:18:55 -0500

    populating matrix

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,23 +4,31 @@ public class RunMatrix {
   public static void main(String[] args) {
     int row;
     int col;
-    Scanner input = new Scanner(System.in);
+    int currentValue = 1;
+    Scanner scan = new Scanner(System.in);
     System.out.println("Please enter the size of your matrix");
-    int size = input.nextInt();
+    int size = scan.nextInt();
     System.out.println("Your matrix is " + size + " x " + size);
-  {
+
+    System.out.println("Printing matrix:");
     int [][] table = new int [size] [size];
-
-    // for (row = 0; row < table.length; row++)
-    //   for (col = 0; col < table[row].length; col++)
-    //     table[row][col] = row * 10 + col;
-
-    for (row = 0; row < table.length; row++)
-    {
-      for (col = 0; col < table[row].length; col++)
+    for (row = 0; row < table.length; row++) {
+      for (col = 0; col < table[row].length; col++) {
         System.out.print(table[row][col] + "\t");
+      }
       System.out.println();
-    }
-  }
+   }
+    System.out.println();
+    System.out.println("Populating matirx...");
+    for (row = 0; row < table.length; row++) {
+      for (col = 0; col < table[row].length; col++) {
+        table[row][col] = currentValue++;
+        System.out.print(table[row][col] + "\t");
+      }
+      System.out.println();
+   }
+    System.out.println();
+    System.out.println("Flipping matrix:");
+   scan.close();
   }
 }
